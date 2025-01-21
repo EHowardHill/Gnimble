867fdf973fb20152364edfd4781d2ea5687ed014
Ethan Hill <ethan.hill1999@outlook.com>
2025-01-21 17:00:10 -0600

    basically done

--- HTML/app.py
+++ HTML/app.py
@@ -2,6 +2,21 @@ from flask import Flask, render_template, request
 from os import path, listdir
 from json import load, dump
 
+import socket
+
+def get_intranet_ip():
+    """Gets the IP address of the current machine on the intranet."""
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        s.connect(("8.8.8.8", 80))  # Connect to a known external IP
+        ip_address = s.getsockname()[0]
+        s.close()
+        return ip_address
+    except Exception:
+        return None
+    
+IPAddr = get_intranet_ip()
+
 app = Flask(__name__)
 
 @app.route('/')
@@ -14,7 +29,7 @@ def menu():
             print(data["title"])
             stories.append({"ref": data["ref"], "title": data["title"]})
     
-    return render_template('menu.html', stories=stories)
+    return render_template('menu.html', stories=stories, ip=IPAddr)
 
 @app.route('/edit')
 def edit():
@@ -25,6 +40,40 @@ def edit():
     content = data["content"]
 
     return render_template('index.html', title=title, content=content, ref=ref)
+
+@app.route('/rename', methods=["POST"])
+def rename():
+    ref = request.json.get("ref")
+    title = request.json.get("title")
+
+    with open(path.join("stories", ref + ".json"), "r") as f:
+        data = load(f)
+
+    data["title"] = title
+
+    with open(path.join("stories", ref + ".json"), "w") as f:
+        dump(data, f)
+
+    return {
+        "success": 1
+    }
+
+@app.route('/create', methods=["POST"])
+def rename():
+    ref = request.json.get("ref")
+    title = request.json.get("title")
+
+    with open(path.join("stories", ref + ".json"), "r") as f:
+        data = load(f)
+
+    data["title"] = title
+
+    with open(path.join("stories", ref + ".json"), "w") as f:
+        dump(data, f)
+
+    return {
+        "success": 1
+    }
 
 @app.route('/save', methods=["POST"])
 def save():
